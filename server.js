55183c0fbbf90468e94fbd681232d9c987cb30a8
Bane <nasirh831@gmail.com>
2020-10-13 11:32:38 +0530

    auth system

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
 var bodyParser = require('body-parser');
+const passport=require('passport')
 
 app.use(bodyParser.json());
 
@@ -28,6 +29,7 @@ connection.once('open',()=>{
     console.log('Connection failed...')
 })
 
+
 let mongoStore = new MongoDbStore({
                 mongooseConnection:connection,
                 collection:'sessions'
@@ -41,14 +43,24 @@ app.use(session({
     cookie:{maxAge: 1000*60*60*24}
 }))
 
+//passport config
+
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended : false }))
+app.use(express.json())
 
 //Global middleware
 
 app.use((req,res,next) =>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
